da22089a01fd22881eccbbafb28d4410c95b7e20
lokesh <23p61a0540@vbithyd.ac.in>
2026-01-30 11:38:47 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ class Hello
     public static void main(String args[])
     {
       System.out.println("hello world");
-      system.out.println("WELCOME TO VBIT")
+      System.out.println("WELCOME TO VBIT");
     }
   }
